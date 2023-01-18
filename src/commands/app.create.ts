e0e2147865ceabfdb6d2cd22c7d615e8d32f9c5b
mmdzov <mzov939@gmail.com>
2023-01-18 04:46:28 +0330

    Added some dependencies + Fixed create bugs

--- src/commands/app.create.ts
+++ src/commands/app.create.ts
@@ -1,14 +1,15 @@
 #! /usr/bin/env node
 
+import os from "os";
 import { Argv } from "yargs";
 import chalk from "chalk";
 import shell from "shelljs";
-import { cwd } from "process";
 import { join } from "path";
 import fs from "fs-extra";
-import packageJson from "../shared/packageJson";
 import Spinner from "../shared/Spinner";
 import execSync from "../shared/execSync";
+import fsExtra from "fs-extra";
+import { Ora } from "ora";
 
 const appCreate = (cli: Argv) => {
   cli.command(
@@ -16,52 +17,95 @@ const appCreate = (cli: Argv) => {
     "Create Framework Template",
     (yargs) => {},
     async (args) => {
-      const withReact = args?.react || ""
+      const withReact = args?.react || "";
 
       const appName = ((args?.app_name as string) ?? "").trim();
 
-      //   if (appName === ".") {
-      //   }
+      let spin = {} as Ora;
 
-      const spin = new Spinner().start("Waiting for Initializing...");
+      try {
+        spin.clear();
+      } catch (e) {}
 
-      const tempDir = join(__dirname, "../../", "templates");
+      setTimeout(() => {
+        spin = new Spinner().start("Waiting for Initializing source...");
+      }, 100);
 
-      shell.mkdir(appName);
-      shell.cd(appName);
-      shell.mkdir("src");
+      if (appName !== ".") {
+        fs.mkdirSync(appName);
+        shell.cd(appName);
+      }
 
-      await execSync("npm init -y", { silent: true, async: true });
-      await execSync("npm i @isorm/core express", {
-        silent: true,
-        async: true,
-      });
-      await execSync(
-        "npm i @types/node @types/express typescript ts-node-dev -D",
+      await shell.exec(
+        "git clone https://github.com/isorm/templates.git Initializing",
         {
-          silent: true,
           async: true,
+          silent: true,
+        },
+        async (code, stdout, stderr) => {
+          shell.cd("Initializing");
+
+          const initDir = shell.pwd().stdout;
+
+          fsExtra.copySync(
+            join(initDir, withReact ? "react-isorm" : "isorm"),
+            join(initDir, "../"),
+          );
+
+          shell.cd("..");
+
+          const rootDir = shell.pwd().stdout;
+
+          fsExtra.removeSync(join(initDir));
+
+          const tsConfigData = fs.readFileSync(
+            join(rootDir, "tsconfig.json"),
+            "utf8",
+          );
+
+          const tsConfig = JSON.parse(tsConfigData);
+
+          tsConfig.include = tsConfig.include.filter(
+            (item: string) => item !== "../index.d.ts",
+          );
+
+          fs.writeFileSync(
+            join(rootDir, "tsconfig.json"),
+            JSON.stringify(tsConfig, null, 2),
+          );
+
+          const packageJsonData = fs.readFileSync(
+            join(rootDir, "package.json"),
+            "utf8",
+          );
+
+          const packageJson = JSON.parse(packageJsonData);
+
+          packageJson.name =
+            appName !== "." ? appName : rootDir.split("\\").slice(-2)[0];
+
+          packageJson.author = os.hostname();
+
+          fs.writeFileSync(
+            join(rootDir, "package.json"),
+            JSON.stringify(packageJson, null, 2),
+          );
+
+          spin.stop();
+
+          spin.start("Installing Dependencies...");
+
+          await execSync("npm i", {
+            silent: true,
+          });
+
+          spin.stop();
+
+          console.log(chalk.blueBright("Project was created successfully :)"));
         },
       );
-      await execSync("tsc --init", { silent: true, async: true });
-
-      fs.writeFileSync(
-        join(cwd(), "tsconfig.json"),
-        fs.readFileSync(join(tempDir, "tsconfig.json"), "utf8"),
-      );
-      fs.copySync(join(tempDir, "src"), join(cwd(), "src"), {
-        overwrite: true,
-      });
-
-      packageJson();
-
-      spin.stop();
-      shell.cd("..");
-      console.log(chalk.blueBright("Project was created successfully :)"));
     },
   );
-
-
 };
 
 export default appCreate;
